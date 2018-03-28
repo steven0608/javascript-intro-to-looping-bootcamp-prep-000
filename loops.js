1a1b563ab664f199fc834b89dc164390eeafafc8
Steven Chen <ysteven.chen2@gmail.com>
2018-03-28 22:16:08 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -16,4 +16,11 @@ function whileLoop(n){
   return "done"
 }
 
-function doWhileLoop(array){}
+function maybeTrue() {
+  return Math.random() >= 0.5}
+function doWhileLoop(array){
+do { delete array}
+
+while array.length>0&&maybeTrue();
+return array;  
+}
