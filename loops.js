ae947317def9d8d53ee95f3f23172b24ef079192
Steven Chen <ysteven.chen2@gmail.com>
2018-03-28 22:22:56 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -9,19 +9,4 @@ function forLoop(array){
   return array;
 }
 
-function whileLoop(n){
-  while (n>0){
-    console.log(--n);
-  }
-  return "done"
-}
 
-function doWhileLoop(array){
-do {function maybeTrue() {
-  return Math.random() >= 0.5}
-
-while (array.length>0&&maybeTrue(array)){
-  array.shift([1])
-}
-return array;
-}
