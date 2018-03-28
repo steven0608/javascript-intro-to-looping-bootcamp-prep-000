8288655ca9e288ce72183abacdcaa4dea160a8f5
Steven Chen <ysteven.chen2@gmail.com>
2018-03-28 20:34:16 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,5 +1,6 @@
 function forLoop(array){
-for (var i=0;i<25;i++){
-  console.log("I am "+i+" strange loops");
+for (var i=0;i<25;i++){if(i===1);
+  console.log("I am "+i+" strange loops.");
+  else console.log("I am ${i} strange loops.")
 }
 }
